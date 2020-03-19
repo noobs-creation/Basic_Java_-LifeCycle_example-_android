8fa4c8406658800c4489738f9e1485eda337b863
noobs-creation <noobs-creation@users.noreply.github.com>
2020-03-19 13:36:35 +0530

    starting android apps with java

--- app/src/main/java/com/example/basicjavalifecycle/MainActivity.java
+++ app/src/main/java/com/example/basicjavalifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.basicjavalifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Lifecycle event :", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("Lifecycle event :", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("Lifecycle event :", "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("Lifecycle event :", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("Lifecycle event :", "onDestroy");
     }
 }
